cc66e34a2ce26f204bd370706a02529d5a4788ff
Omerking-AZE <omerqedirli@gmail.com>
2026-08-15 18:42:04 +0400

    Add initial smoke test

---
+++ tests/test_smoke.py
@@ -0,0 +1,11 @@
+﻿import unittest
+
+
+class TestProjectSmoke(unittest.TestCase):
+
+    def test_project_imports(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
